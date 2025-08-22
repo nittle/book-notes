f9754b90e31acfca8127a8c8bf9052490ccb49ce
goose <danikdruk20@gmail.com>
2025-08-22 10:26:01 +0300

    Initialize postgres database

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import e from "express";
+import PG from "pg";
+import fs from "fs";
 
 const app = e();
 const port = 3000;
 
+// Reads db-config.json and creates pg.client()
+const dbConfig = JSON.parse(fs.readFileSync("db-config.json", "utf8"));
+const db = new PG.Client(dbConfig);
+db.connect();
+console.log((await db.query("SELECT * FROM record")).rows);
+
+async function getRecords() {
+  const records = (await db.query("SELECT * FROM record")).rows;
+  return records;
+}
+
 app.use(e.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get("/", async (req, res) => {
+  const records = await getRecords();
+  res.render("index.ejs", { records: records });
 });
 
 app.listen(port, () => {
