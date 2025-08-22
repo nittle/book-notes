5e3f736899247767db1ca4e0fb00ad6ff9ece501
goose <danikdruk20@gmail.com>
2025-08-22 15:06:42 +0300

    Add search logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import e from "express";
 import PG from "pg";
 import fs from "fs";
+import bodyParser from "body-parser";
 
 const app = e();
 const port = 3000;
@@ -9,7 +10,6 @@ const port = 3000;
 const dbConfig = JSON.parse(fs.readFileSync("db-config.json", "utf8"));
 const db = new PG.Client(dbConfig);
 db.connect();
-console.log((await db.query("SELECT * FROM record")).rows);
 
 async function getRecords() {
   const records = (await db.query("SELECT * FROM record")).rows;
@@ -20,7 +20,17 @@ async function getRecordById(id) {
   const record = records.find((record) => record.id === id);
   return record;
 }
+async function findRecordsByTitle(title) {
+  const records = (
+    await db.query(
+      "SELECT * FROM public.record WHERE LOWER(title) LIKE '%' || $1 || '%'",
+      [title]
+    )
+  ).rows;
+  return records;
+}
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(e.static("public"));
 
 app.get("/", async (req, res) => {
@@ -33,11 +43,16 @@ app.get("/", async (req, res) => {
 app.get("/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const record = await getRecordById(id);
-  console.log(record);
 
-  console.log(id);
   if (record) res.render("book.ejs", { record: record });
   else res.sendStatus(404);
+});
+
+app.post("/search", async (req, res) => {
+  const searchQuery = req.body.query;
+  const records = await findRecordsByTitle(searchQuery);
+
+  res.render("index.ejs", { records: records });
 });
 
 app.listen(port, () => {
